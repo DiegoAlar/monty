6d5662db640d7d3d1d8f9f3adcd1cc41788be574
DiegoAlar <diegoaav@hotmail.com>
2020-01-05 11:48:28 -0500

    adding functionality to function swap

--- doubly_linked_lists_fns.c
+++ doubly_linked_lists_fns.c
@@ -105,7 +105,15 @@ void pop(stack_t **stack, unsigned int line_number)
   */
 void swap(stack_t **stack, unsigned int line_number)
 {
-	*stack = NULL;
-	line_number = line_number;
+	int temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+	}
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+
 }
 
